17203001fd7e7ddc80969a14094249e17bedccd5
elperez750 <elperez750@gmail.com>
2025-11-08 19:00:51 -0800

    Read from input file. Created MyVector Objects and stored them in linked list

--- src/main/java/package4/MyVector.java
+++ src/main/java/package4/MyVector.java
@@ -3,11 +3,214 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package package4;
+import java.util.ArrayList;
 
 /**
  *
- * @author elper
+ * @author elper2
  */
+
+
+/**
+* Project: Project1
+* Completion time: ? hours (fill in the time it took you to write the program)
+*
+* Honor Code: “I pledge that I have neither given nor received help from anyone
+* other than the instructor or the TAs for all program components included here.”
+*/
+
+
 public class MyVector {
+      private final ArrayList<Double> initvalues;
+      private static final double EPSILON = 1e-9;
+
     
+      
+    
+    public MyVector (double[] initvalues){
+        
+        this.initvalues = new ArrayList<>(initvalues.length);
+        for (double val: initvalues) {
+            this.initvalues.add(val);
+        }
+      
+          }
+    
+    
+    public MyVector(MyVector original) {
+        this.initvalues = new ArrayList<>(original.initvalues);
+    }
+    
+    
+ 
+    
+    public int size() {
+        
+        return this.initvalues.size();
+        
+    }
+    
+    public double get(int index) {
+        return this.initvalues.get(index);
+    }
+    
+    
+    public MyVector plus(MyVector addVector)  {
+        int size = this.initvalues.size();
+        
+        if (size != addVector.size()) {
+            throw new IllegalArgumentException("Vectors must be the same size");
+
+            /*return ("Vectors must be same size!!");*/
+        }
+        
+        
+        
+        double [] resultArray = new double[size];
+        
+        
+        
+        for (int i =0; i< size; i++) {
+           resultArray[i] = this.initvalues.get(i) + addVector.get(i);
+                       
+        }
+        
+        
+        MyVector resultVector = new MyVector(resultArray);
+        
+        return resultVector;
+        
+     
+    }
+    
+    
+    public MyVector sub(MyVector subVector) {
+        int size = this.initvalues.size();
+
+        if (size != subVector.size()) {
+            throw new IllegalArgumentException("Vectors must be the same size");
+
+            
+        }
+        
+        
+        
+        double[] resultArray = new double[size];
+        
+        
+        for (int i=0; i< size; i++) {
+            resultArray[i] = this.initvalues.get(i) - subVector.get(i);
+            
+        }
+        
+        
+        
+        MyVector resultVector = new MyVector(resultArray);
+        
+        return resultVector;
+                
+        
+    }
+    
+    
+    
+    public MyVector scaledBy(double scaleFactor) {
+        int size = this.initvalues.size();
+        
+        double[] scaledArray = new double[size];
+        
+        for (int i = 0; i < size; i++) {
+            scaledArray[i] = this.initvalues.get(i) * scaleFactor;
+        }
+        
+        
+        MyVector scaledVector = new MyVector(scaledArray);
+        
+        return scaledVector;
+        
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        
+        int size = this.initvalues.size();
+        for (int i =0; i < size; i++) {
+            sb.append("This is value number ").append(i + 1).append(": ");
+            sb.append(String.format("%.2f", initvalues.get(i)));
+            
+            
+            if (i != size - 1) {
+                sb.append(", ");
+            }
+             
+        }
+        
+        sb.append("]");
+        
+        return sb.toString();
+    }
+    
+    
+    
+    public double dot (MyVector dotVector) {
+        int size = this.initvalues.size();
+       
+        if (size != dotVector.size()) {
+            throw new IllegalArgumentException("Vectors must be the same size");
+
+        }
+        
+        double dotProduct = 0;
+        
+      
+        
+        for (int i =0; i< size; i++) {
+            dotProduct += (this.initvalues.get(i) * dotVector.get(i));
+           
+        }
+        
+        
+        return dotProduct;
+       
+        
+    }
+    
+    
+    
+    public double abs() {
+        double dotProduct = this.dot(this);
+  
+        return Math.sqrt(dotProduct);
+        
+
+    }
+    
+    
+    public boolean equals(MyVector comparisonVector) {
+
+        
+        int size = this.initvalues.size();
+        
+        
+        
+        
+        if (size != comparisonVector.size()) {
+            throw new IllegalArgumentException("Vectors must be the same size");
+        }
+        
+       
+        
+        for (int i = 0; i < size; i++) {
+            if (Math.abs(this.initvalues.get(i) - comparisonVector.get(i)) > EPSILON) {
+                return false;
+            }
+}
+        
+        return true;
+        
+    
+    }
 }
--- src/main/java/package4/Project4.java
+++ src/main/java/package4/Project4.java
@@ -3,14 +3,103 @@
  */
 
 package package4;
+import java.util.*;
+import java.io.*;
 
 /**
- *
- * @author elper
- */
+* Project: Project4
+* Completion time: 5 hours (fill in the time it took you to write the program)
+*
+* Honor Code: “I pledge that I have neither given nor received help from anyone
+* other than the instructor or the TAs for all program components included here.”
+*/
+
+
 public class Project4 {
+    
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LinkedList vectors = new LinkedList<MyVector>();
+        MyVector mv;
+
+        
+ 
+        
+        File myFile = new File("src/input.txt");
+        try {
+            Scanner fileScanner = new Scanner (myFile);
+            
+            int arraySize = fileScanner.nextInt();
+            
+            while (fileScanner.hasNextDouble()) {
+                double[] array = new double[arraySize];
+
+                for (int i = 0; i < arraySize; i++) {
+                    array[i] = fileScanner.nextDouble();
+                        
+
+                }
+                mv = new MyVector(array);
+                
+                vectors.add(mv);
+                
+            }
+            
+
+        }
+        
+ 
+        catch (FileNotFoundException e) {
+            System.out.println("Error: File not found!");
+            
+        }
+        
+        MyVector firstVector = (MyVector) vectors.get(0);
+        System.out.println(firstVector.get(1));
+        
+        
+        
+       
+        
+
+            
+   
+        
+        
+        
+        
+//        MyVector v1 = new MyVector(new double[]{1.0, 2.0, 3.0});
+//        MyVector v2 = new MyVector(new double[] {2.0, 4.0, 6.0});
+//        MyVector v3 = new MyVector(new double[]{3.0, 6.0, 9.0});
+//        
+//        
+//        
+//        strings.add("Hello");
+//        strings.add("Goodbye");
+//        strings.add("I need you");
+//        
+//        
+//        ListIterator<String> itrForwards = strings.listIterator();
+//        ListIterator<String> itrBackwards = strings.listIterator(strings.size());
+//        
+//        
+//        // Getting all elements starting with beginning of the list
+//        while (itrForwards.hasNext()) {
+//            System.out.println(itrForwards.next());
+//            
+//        }
+//        
+//        
+//        // Getting all elements starting at the end
+//        while (itrBackwards.hasPrevious()) {
+//            System.out.println(itrBackwards.previous());
+//            
+//        }
+//        
+        
+        
+        
+        
+        
     }
 }
