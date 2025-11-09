6c8954fcdf2166fe94d0de347723489acda56f32
elperez750 <elperez750@gmail.com>
2025-11-08 16:54:39 -0800

    First commit

---
+++ src/main/java/package4/Project4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package package4;
+
+/**
+ *
+ * @author elper
+ */
+public class Project4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
